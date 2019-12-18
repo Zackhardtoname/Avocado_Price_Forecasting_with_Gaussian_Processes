0e47ac6b7a844de1a6321563587d2311a0bd8feb
braceal <braceal@umich.edu>
2019-12-18 02:04:14 -0500

    oraganized r code

--- src/models/unused.py
+++
@@ -1,5 +0,0 @@
-# train = df[(df.index.year != 2016) & (df.index.year != 2018)].AveragePrice.values.reshape(-1, 1)
-# ground_truth = df[df.index.year == 2016].AveragePrice.values.reshape(-1, 1)
-
-k = y_gpr + test.mean() - y_gpr.mean()
-sklearn.metrics.r2_score(k, test)
