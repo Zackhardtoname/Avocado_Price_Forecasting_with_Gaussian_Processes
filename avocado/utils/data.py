1c9f48f71efa6b73858b1146144cb34e1fad07c2
braceal <braceal@umich.edu>
2019-12-18 07:45:56 -0500

    added comments to utility scirpts and helper functions

--- avocado/utils/data.py
+++ avocado/utils/data.py
@@ -21,18 +21,23 @@ def get_data(type_='organic', region='TotalUS'):
 
     """
 
+    # Define paths to data
     train_path = os.path.join('.', 'data', type_, 'train', f'{region}.csv')
     test_path = os.path.join('.', 'data', type_, 'test', f'{region}.csv')
 
+    # Read data into pandas dataframe
     df_train = pd.read_csv(train_path, index_col='Date')
     df_test = pd.read_csv(test_path, index_col='Date')
 
+    # Collect underlying np arrays from dataframes
     y_train = df_train.AveragePrice.values.reshape(-1, 1)
     y_test = df_test.AveragePrice.values.reshape(-1, 1)
 
+    # Generate weekly-spaced data for each price observation (incremented by 7 starting from 1)
     X_train = np.array([7 * i + 1 for i in range(len(y_train))]).reshape(-1, 1)
     X_test = np.array([7 * i + 1 for i in range(len(y_train), len(y_train) + len(y_test))]).reshape(-1, 1)
 
+    # Join train/test dataframes into one for plotting purposes
     df = pd.concat((df_train, df_test), join="inner")
     df.index = pd.to_datetime(df.index)
 
